d4fa5c02fdfe19757d5d34b566722b43fd1c927b
Bastian Bechtle <bastian.bechtle@goldbek-verlag.de>
2021-06-17 17:08:02 +0200

    TRAC-761: Initial Unit Test for Local Storage, wip

---
+++ tests/cmp_interaction_tracking.test.js
@@ -0,0 +1,19 @@
+const functions = require('../extensions/cmp_interaction_tracking');
+const localStorage = require('../tests/browserMocks');
+
+let data = {}
+
+
+    it( 'setItem mock fn', () => {
+        const mockFn = jest.fn( localStorage.setItem );
+        localStorage.setItem = mockFn;
+        localStorage.setItem( 'cmp_ab_desc', 'cmp_ab_id','cmp_ab_bucket' );
+        expect( mockFn ).toHaveBeenCalledTimes( 1 );
+        //const processMessageDataFn = functions.processMessageData(data)
+        //processMessageDataFn(data);
+        //expect(functions.processMessageData(data)).toBe(typeof data)
+    } );
+
+
+
+

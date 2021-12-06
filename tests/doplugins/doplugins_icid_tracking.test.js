2f1ef7c8e84f3aa7e3239c93bfc2603bc038f09a
Heiko Kampe <heiko.kampe@posteo.de>
2021-12-06 17:44:51 +0100

    TRAC-1164: move ICID tracking to separate function

---
+++ tests/doplugins/doplugins_icid_tracking.test.js
@@ -0,0 +1,34 @@
+const doPluginsGlobal = require('../../extensions/doPlugins_global');
+const {createWindowMock} = require('../mocks/browserMocks');
+
+describe('setICIDTrackingCode', () => {
+    let s;
+    beforeEach(() => {
+        // Create a fresh window mock for each test.
+        const windowMock = createWindowMock();
+        jest.spyOn(global, 'window', 'get')
+            .mockImplementation(() => (windowMock));
+
+        s = {
+            ...doPluginsGlobal.s
+        };
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should assign the value of the ICID tracking value to eVar78 and eVar79', function () {
+        const anyICID = 'any-icid';
+        window.location.search = `?icid=${anyICID}`;
+        doPluginsGlobal.ICIDTracking.setVariables(s);
+        expect(s.eVar78).toBe(anyICID);
+        expect(s.eVar79).toBe(anyICID);
+    });
+
+    it('should assign empty string to eVar78 and eVar79 if there is no ICID tracking value', function () {
+        doPluginsGlobal.ICIDTracking.setVariables(s);
+        expect(s.eVar78).toBe('');
+        expect(s.eVar79).toBe('');
+    });
+});

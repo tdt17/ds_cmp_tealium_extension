3442a7ccb79d52d2c257827f2824bda43050f6a5
Nicole Emer <nicole.emer@axelspringer.com>
2022-05-23 12:01:31 +0200

    change to is_page_live_article = '1'

--- extensions/doPlugins_global.js
+++ extensions/doPlugins_global.js
@@ -474,8 +474,8 @@ s._bildPageNameObj = {
     },
 
     isLive: function () {
-        return !!this.isDocTypeArticle() && !!window.utag.data.page_cms_path
-            && window.utag.data.page_cms_path.indexOf('im-live-ticker') !== -1;
+        return !!this.isDocTypeArticle() && !!window.utag.data.is_page_live_article
+            && window.utag.data.is_page_live_article === '1';
     },
  
     isLiveSport: function () {

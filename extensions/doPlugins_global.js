f226cc10d86f6d5a7ccd92e7d3e25f5a7257438f
Nicole Emer <nicole.emer@axelspringer.com>
2022-07-14 17:16:00 +0200

    event102

--- extensions/doPlugins_global.js
+++ extensions/doPlugins_global.js
@@ -255,8 +255,6 @@ s._articleViewTypeObj = {
             return 'event76'; // Bild home desktop recommendation
         } else if (this.isFromHomeMobileWithReco()) {
             return 'event77'; // Bild home mobile recommendation
-        } else if (this.isFromArticleWithReco()) {
-            return 'event102'; // Article recommendation
         } else {
             return 'event27';
         }
@@ -317,8 +315,8 @@ s._articleViewTypeObj = {
             articleViewType = 'event24'; // Search
         } else if (trackingValue.startsWith('social')) {
             articleViewType = 'event25'; //Social
-        } else if (trackingValue.startsWith('kooperation') || trackingValue.startsWith('affiliate')) {
-            articleViewType = 'event23'; //Other Internal
+        } else if (trackingValue.startsWith('kooperation.article.outbrain.')) {
+            articleViewType = 'event102'; //Outbrain Reco at Articles
         }
         return articleViewType;
     },

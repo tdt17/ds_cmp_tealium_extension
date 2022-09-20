82f34b4c86cff8e12668ce3db151079a1720c401
Ana Kikerec <ana.kikerec@axelspringer.com>
2022-09-20 12:11:50 +0200

    previous page implementation in welt apps

--- extensions/doPlugins_welt_apps_android.js
+++ extensions/doPlugins_welt_apps_android.js
@@ -1,6 +1,14 @@
 /* eslint-disable */
 const s = window.s || {};
 
+// START: Pre-defined Adobe Plugins
+/* istanbul ignore next */
+/* Adobe Consulting Plugin: getPreviousValue v3.0 */
+s.getPreviousValue = function(v,c){var k=v,d=c;if("-v"===k)return{plugin:"getPreviousValue",version:"3.0"};var a=function(){if("undefined"!==typeof window.s_c_il)for(var c=0,b;c<window.s_c_il.length;c++)if(b=window.s_c_il[c],b._c&&"s_c"===b._c)return b}();"undefined"!==typeof a&&(a.contextData.getPreviousValue="3.0");window.cookieWrite=window.cookieWrite||function(c,b,f){if("string"===typeof c){var h=window.location.hostname,a=window.location.hostname.split(".").length-1;if(h&&!/^[0-9.]+$/.test(h)){a=2<a?a:2;var e=h.lastIndexOf(".");if(0<=e){for(;0<=e&&1<a;)e=h.lastIndexOf(".",e-1),a--;e=0<e?h.substring(e):h}}g=e;b="undefined"!==typeof b?""+b:"";if(f||""===b)if(""===b&&(f=-60),"number"===typeof f){var d=new Date;d.setTime(d.getTime()+6E4*f)}else d=f;return c&&(document.cookie=encodeURIComponent(c)+"="+encodeURIComponent(b)+"; path=/;"+(f?" expires="+d.toUTCString()+";":"")+(g?" domain="+g+";":""),"undefined"!==typeof cookieRead)?cookieRead(c)===b:!1}};window.cookieRead=window.cookieRead||function(c){if("string"===typeof c)c=encodeURIComponent(c);else return"";var b=" "+document.cookie,a=b.indexOf(" "+c+"="),d=0>a?a:b.indexOf(";",a);return(c=0>a?"":decodeURIComponent(b.substring(a+2+c.length,0>d?b.length:d)))?c:""};var l;d=d||"s_gpv";a=new Date;a.setTime(a.getTime()+18E5);window.cookieRead(d)&&(l=window.cookieRead(d));k?window.cookieWrite(d,k,a):window.cookieWrite(d,l,a);return l};
+/******************************************** END CODE TO DEPLOY ********************************************/
+/* eslint-enable */
+// END: Pre-defined Adobe Plugins
+
 s._setPageAgeForCheckout = function () {
     if (typeof window.utag.data.adobe_docType !== 'undefined' && window.utag.data.adobe_docType === 'article') {
         window.utag.loader.SC('utag_main', { 'pa': window.utag.data.screen_agePublication + ';exp-session' });
@@ -48,6 +56,9 @@ s.doPlugins = function (s) {
     s.eVar184 = new Date().getHours().toString();
     s.eVar181 = new Date().getMinutes().toString();
     s.eVar185 = window.utag.data.myCW || '';
+
+    s.eVar33 = s.getPreviousValue(s.pageName);
+    s.prop61 = s.getPreviousValue(s.pageName);
 
     s._setPageSection(s);
 };
